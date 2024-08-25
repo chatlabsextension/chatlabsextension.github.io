17ab520b80b2e7df74b344101b05e8fd20cd38e0
chatlabsextension <info@chatlabs.se>
2024-08-25 10:05:02 +0200

    Update extensions.js

--- extensions.js
+++ extensions.js
@@ -1,1 +1,12 @@
-
+export const PlaceholderExtension = {
+  name: "Placeholder",
+  type: "effect",
+  match: ({ trace }) =>
+    trace.type === "ext_placeholder" || trace.payload.name === "ext_placeholder",
+  effect: ({ trace }) => {
+    const chatDiv = document.getElementById("voiceflow-chat");
+    const shadowRoot = chatDiv.shadowRoot;
+    const textarea = shadowRoot.querySelector("textarea");
+    textarea.placeholder = trace.payload.placeholder;
+  },
+};
