dab713a277b10d7ff35122e3fcfc4b83a72befa3
chatlabsextension <info@chatlabs.se>
2024-08-26 23:20:26 +0200

    Update extensions.js

--- extensions.js
+++ extensions.js
@@ -1,12 +1,12 @@
 export const PlaceholderExtension = {
-  name: "Placeholder",
-  type: "effect",
-  match: ({ trace }) =>
-    trace.type === "ext_placeholder" || trace.payload.name === "ext_placeholder",
-  effect: ({ trace }) => {
+  type: 'extension',
+  extensionType: 'effect',
+  name: 'Placeholder',
+  match: (message) => message.type === 'ext_placeholder' || message?.payload?.name === 'ext_placeholder',
+  effect: (message) => {
     const chatDiv = document.getElementById("cd-widget");
     const shadowRoot = chatDiv.shadowRoot;
-    const textarea = shadowRoot.querySelector("textarea");
-    textarea.placeholder = trace.payload.placeholder;
+    const inputField = shadowRoot.querySelector(".cd-widget-text-input input");
+    inputField.placeholder = message.payload.placeholder;
   },
 };
